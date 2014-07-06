25de92ecfed493cda5b51df4719f444a3b2937ab
Casey Bergman <casey.bergman@manchester.ac.uk>
2014-07-06 21:02:50 +0100

    removed padding steps from fixEmbl script

--- scripts/fixEmblFile.pl
+++ scripts/fixEmblFile.pl
@@ -2,11 +2,8 @@
 
 # usage
 if ($#ARGV != 0) {
-        die "Usage: TE_to_EMBL.pl transposon_sequence_set.v.9.42.txt\n";
+        die "Usage: fixEmblFile.pl transposon_sequence_set.embl.txt\n";
 }
-
-#lookup hash table of SO terms converted to EMBL feature keys
-#my %SO_to_EMBL = qw(CDS CDS dinucleotide_repeat_microsatellite_feature repeat_unit direct_repeat repeat_unit five_prime_LTR repeat_unit five_prime_UTR 5'UTR intron intron inverted_repeat repeat_unit long_terminal_repeat repeat_unit polyA_sequence misc_RNA polyA_signal_sequence misc_RNA polyA_site polyA_site primer_binding_site primer_bind pseudogene misc_RNA region misc_structure RR_tract repeat_unit start_codon misc_RNA TATA_box TATA_signal terminal_inverted_repeat repeat_unit tetranucleotide_repeat_microsatellite_feature repeat_unit three_prime_LTR repeat_unit three_prime_UTR 3'UTR transcription_start_site misc_signal);
 
 my $feature = '';
 my $id = '';
@@ -19,39 +16,47 @@ my $nextCoords = '';
 while ($line = <>) {
 
 	#if line has SO feature, get relevant info
-	if ($line =~ /SO_feature\s+(\S+)\s\;\s(\S+):(\S+)/) {
+	if ($line =~ /SO_feature\s+(\S+)\s\;\s(SO:\S+):(\S+)/) {
 		$feature = $1;
 		$id = $2;
 		$coords = $3;
 
 	    #find length of SO feature string and add padding to make columns 6-20 in EMBL file
-		my $feature_string_length = length $feature;
-		my $padding = '';
-		for (my $i=$feature_string_length; $i<100; $i++) {
-			$padding .= " ";
-		}			
+	    # this is no longer necessary since padding required for long SO strings exceed EMBL format
+	    # embl converter doesn't require fixed width or embl tags
+	    
+#		my $feature_string_length = length $feature;
+#		my $padding = '';
+#		for (my $i=$feature_string_length; $i<100; $i++) {
+#			$padding .= " ";
+#		}			
 
 		#convert position of start codons for complete orfs on the + strand	
 		if ($feature eq 'start_codon') {
 			$nextLine = <>;
 			if ($nextLine =~ /SO_feature\s+(\S+)\s\;\s(\S+):(\S+)/) {
+			
+			#FT   SO_feature      CDS ; SO:0000316:join(155..447,512..1179,1242..1967,2159..2733)
+			#FT   SO_feature      CDS ; SO:0000316:394..1440
+
+
 				$nextFeature = $1;
 				$nextId = $2;
 				$nextCoords = $3;
 			}
 			
-			$next_feature_string_length = length $nextFeature;
-			$next_padding = '';
+#			$next_feature_string_length = length $nextFeature;
+#			$next_padding = '';
 			
-			for (my $i=$next_feature_string_length; $i<100; $i++) {
-				$next_padding .= " ";
-			}
+#			for (my $i=$next_feature_string_length; $i<100; $i++) {
+#				$next_padding .= " ";
+#			}
 			
 			if ($nextCoords =~ /</ || $nextCoords =~ />/ || $nextCoords =~ /complement/) {
 				#print "$line";
 				#print "feature is $feature\tcoords are $coords\tnextFeature is $nextFeature\tnextCoords are $nextCoords\n";
 				#print "incomplete ORF\n\n";
-				print "FT   $nextFeature$next_padding$nextCoords\n";
+				print "FT   $nextFeature     $nextCoords\n";
 				print "FT                   /db_xref=\"$nextId\"\n";
 			}
 			
@@ -60,19 +65,16 @@ while ($line = <>) {
 				#print "$line";
 				#print "feature is $feature\tcoords are $coords\tnextFeature is $nextFeature\tnextCoords are $nextCoords\n";
 				#print "start codon $1 to $2\n\n";
-				print "FT   $feature$padding$1..".($1+2)."\n";
+				print "FT   $feature     $1..".($1+2)."\n";
 				print "FT                   /db_xref=\"$id\"\n";
-				print "FT   $nextFeature$next_padding$nextCoords\n";
+				print "FT   $nextFeature     $nextCoords\n";
 				print "FT                   /db_xref=\"$nextId\"\n";
 
 			}
 		}
 
-		#map SO feature back to EMBL feature keys and make new FT line with dbxref to SO
 		else {
-			#print "FT   $SO_to_EMBL{$feature}$padding$coords\n";
-			print "FT   $feature$padding$coords\n";
-			#print "FT                   /db_xref=\"$id; $feature\"\n";
+			print "FT   $feature     $coords\n";
 			print "FT                   /Ontology_term=\"$id\"\n";
 		}
 	}
