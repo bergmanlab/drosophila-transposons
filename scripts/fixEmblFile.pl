b55b8d60eef64d9c38239b1746f159eb2b510cf8
Casey Bergman <casey.bergman@manchester.ac.uk>
2016-01-13 15:07:03 +0000

    edit to fixEmblFile.pl
    - removed unused code
    - added comments to note what is being fixed in embl file

--- scripts/fixEmblFile.pl
+++ scripts/fixEmblFile.pl
@@ -21,17 +21,9 @@ while ($line = <>) {
 		$id = $2;
 		$coords = $3;
 
-	    #find length of SO feature string and add padding to make columns 6-20 in EMBL file
-	    # this is no longer necessary since padding required for long SO strings exceed EMBL format
-	    # embl converter doesn't require fixed width or embl tags
-	    
-#		my $feature_string_length = length $feature;
-#		my $padding = '';
-#		for (my $i=$feature_string_length; $i<100; $i++) {
-#			$padding .= " ";
-#		}			
-
-		#convert position of start codons for complete orfs on the + strand	
+		# convert the position of start codons from relative to absolute for complete orfs on the + strand	
+		# this conversions requires reorderStartCodons.pl to be run first to make sure that CDS comes after start_codon
+		
 		if ($feature eq 'start_codon') {
 			$nextLine = <>;
 			if ($nextLine =~ /SO_feature\s+(\S+)\s\;\s(\S+):(\S+)/) {
@@ -39,18 +31,10 @@ while ($line = <>) {
 			#FT   SO_feature      CDS ; SO:0000316:join(155..447,512..1179,1242..1967,2159..2733)
 			#FT   SO_feature      CDS ; SO:0000316:394..1440
 
-
 				$nextFeature = $1;
 				$nextId = $2;
 				$nextCoords = $3;
 			}
-			
-#			$next_feature_string_length = length $nextFeature;
-#			$next_padding = '';
-			
-#			for (my $i=$next_feature_string_length; $i<100; $i++) {
-#				$next_padding .= " ";
-#			}
 			
 			if ($nextCoords =~ /</ || $nextCoords =~ />/ || $nextCoords =~ /complement/) {
 				#print "$line";
@@ -62,9 +46,6 @@ while ($line = <>) {
 			
 			elsif ($nextCoords =~ /(\d+)..(\d+)/) {
 				
-				#print "$line";
-				#print "feature is $feature\tcoords are $coords\tnextFeature is $nextFeature\tnextCoords are $nextCoords\n";
-				#print "start codon $1 to $2\n\n";
 				print "FT   $feature     $1..".($1+2)."\n";
 				print "FT                   /Ontology_term=\"$id\"\n";
 				print "FT   $nextFeature     $nextCoords\n";
