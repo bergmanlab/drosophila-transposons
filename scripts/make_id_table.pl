f38a8295c5751f1f39410960c1a02bc50037262d
Shunhua Han <hanshunhua0829@gmail.com>
2018-04-06 16:06:09 -0400

    add input and output path in the script

--- scripts/make_id_table.pl
+++ scripts/make_id_table.pl
@@ -3,8 +3,10 @@
 @lines='';
 # @out='';
 
+open( my $EMBL_DATA, '<', "../current/transposon_sequence_set.embl.txt" );
+open( my $OUTPUT, '>', "../current/te_id_table.tsv" );
 #read line of TE file from standard input
-while ($line = <>) {
+while ($line = <$EMBL_DATA>) {
 	push (@lines, $line);
 }
 
@@ -34,6 +36,9 @@ for ($i=0; $i<$length; $i++) {
         $source_id =~ s/.*\(//g;
         # print "$out\n"
         @out = ($embl_id, $fb_id, $source_id);
-        print join("\t", @out), "\n";
+        print $OUTPUT join("\t", @out), "\n";
     }
 }
+
+close( $EMBL_DATA );
+close( $OUTPUT );
