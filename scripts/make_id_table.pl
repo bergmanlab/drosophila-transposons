5593d9df0709056960b976a4635c8521197f62f3
Shunhua Han <hanshunhua0829@gmail.com>
2018-04-19 21:35:24 -0400

    debug & add argument passing ability

--- scripts/make_id_table.pl
+++ scripts/make_id_table.pl
@@ -1,44 +1,76 @@
 #!/usr/bin/perl
 
-@lines='';
-# @out='';
+use strict; use warnings; use Getopt::Long; use Data::Dumper;
 
-open( my $EMBL_DATA, '<', "../current/transposon_sequence_set.embl.txt" );
-open( my $OUTPUT, '>', "../current/te_id_table.tsv" );
+# ------------------------------------------------------------------
+# GetOptions
+# ------------------------------------------------------------------
+my ($fIn,$fOut);
+GetOptions(
+				"help|?" =>\&USAGE,
+				"o:s"=>\$fOut,
+				"i:s"=>\$fIn,
+				) or &USAGE;
+&USAGE unless ($fIn and $fOut);
+
+sub USAGE {#
+	my $usage=<<"USAGE";
+Description:	this program is used to generate look up table for embl id, flybase id and source id
+Usage:
+  Options:
+  -i <file>  input file,forced
+  -o <file>  output file,forced  
+  -h         Help
+
+USAGE
+	print $usage;
+	exit;
+}
+
+my @lines=();
+my @embl_id=();
+my @fb_id=();
+my @source_id=();
+
+open (IN, '<', $fIn) or die $!;
+open (OUT, '>', $fOut) or die $!;
+
 #read line of TE file from standard input
-while ($line = <$EMBL_DATA>) {
+while (my $line = <IN>) {
 	push (@lines, $line);
 }
 
-$length = @lines;
+my $length = @lines;
+my $idx = -1;
 
-for ($i=0; $i<$length; $i++) {
+for (my $i=0; $i<$length; $i++) {
     if ($lines[$i] =~ /^ID/) {
-        $out = $lines[$i];
-        # print "$out\n";
-        @out = split(' ', $out);
-        $embl_id = @out[1];
-        # print "$out\n"
+        $idx = $idx + 1;
+        my $out = $lines[$i];
+        my @out = split(' ', $out);
+        $embl_id[$idx] = $out[1];
+        push (@fb_id, "");
+        push (@source_id, "");
     }
-	if ($lines[$i] =~ /^DR/) {
-		$out = $lines[$i];
-        # print "$out\n";
-        @out = split('; ', $out);
-        $fb_id = @out[1];
-        # print "$out\n"
-	}
+    if ($lines[$i] =~ /^DR/) {
+        my $out = $lines[$i];
+        my @out = split('; ', $out);
+        $fb_id[$idx] = $out[1];
+    }
     if ($lines[$i] =~ /^FT.*source/) {
-        $out = $lines[$i];
+        my $out = $lines[$i];
         # print "$out\n";
-        @out = split(' ', $out);
-        $source_id = @out[2];
-        $source_id =~ s/:.*//g;
-        $source_id =~ s/.*\(//g;
-        # print "$out\n"
-        @out = ($embl_id, $fb_id, $source_id);
-        print $OUTPUT join("\t", @out), "\n";
+        my @out = split(' ', $out);
+        my $out2 = $out[2];
+        $out2 =~ s/:.*//g;
+        $out2 =~ s/.*\(//g;
+        $source_id[$idx] = $out2;
     }
 }
 
-close( $EMBL_DATA );
-close( $OUTPUT );
+while (@embl_id || @fb_id || @source_id) {
+   print OUT shift(@embl_id) . "\t" . shift(@fb_id) . "\t" . shift(@source_id) . "\n";
+}
+
+close IN;
+close OUT;
--- scripts/replace_id.pl
+++ scripts/replace_id.pl
@@ -1,29 +1,61 @@
 #!/usr/bin/perl
 
-use strict; use warnings;
+use strict; use warnings; use Getopt::Long; use Data::Dumper;
+
+# ------------------------------------------------------------------
+# GetOptions
+# ------------------------------------------------------------------
+my ($fIn,$fOut,$fIn2);
+GetOptions(
+				"help|?" =>\&USAGE,
+				"o:s"=>\$fOut,
+				"i:s"=>\$fIn,
+        "t:s"=>\$fIn2,
+				) or &USAGE;
+&USAGE unless ($fIn and $fOut and $fIn2);
+
+sub USAGE {#
+	my $usage=<<"USAGE";
+Description:	this program is used to replace embl id with flybase id
+Usage:
+  Options:
+  -i <file>  embl file,forced
+  -t <file>  te id file,forced
+  -o <file>  output file,forced  
+  -h         Help
+
+USAGE
+	print $usage;
+	exit;
+}
 
 my %hash;
 
-open( my $TABLE, '<', "../current/te_id_table.tsv" );
+# open( my $TABLE, '<', "../current/te_id_table.tsv" );
+open (IN, '<', $fIn) or die $!;
+open (ID, '<', $fIn2) or die $!;
+open (OUT, '>', $fOut) or die $!;
+
+
 #read line of TE file from standard input
-while (my $line = <$TABLE>) {
+while (my $line = <ID>) {
 	# chomp $_;
     my ($embl_id, $fb_id, $source_id) = split(' ', $line);
     $hash{$embl_id} = $fb_id;
 }
-close( $TABLE );
+close ID;
 
 # print "Key: $_ and Value: $hash{$_}\n" foreach (keys%hash);
 
-open( my $EMBL_DATA, '<', "../current/transposon_sequence_set.embl.txt" );
-open( my $OUTPUT, '>', "../current/transposon_sequence_set.embl.id_replaced.txt" );
+# open( my $EMBL_DATA, '<', "../current/transposon_sequence_set.embl.txt" );
+# open( my $OUTPUT, '>', "../current/transposon_sequence_set.embl.id_replaced.txt" );
 
-while ( <$EMBL_DATA> ) {
+while ( <IN> ) {
   foreach my $key ( sort keys %hash ) {
      s/\b$key\b/$hash{$key}/g;
   }
-  print $OUTPUT $_; 
+  print OUT $_; 
 }
 
-close( $EMBL_DATA );
-close( $OUTPUT );
+close IN;
+close OUT;
