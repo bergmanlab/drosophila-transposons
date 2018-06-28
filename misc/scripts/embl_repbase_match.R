ec12e30ee0103e7b6809e1bd44fc3063527ccd74
Shunhua Han <hanshunhua0829@gmail.com>
2018-06-28 11:43:26 -0400

    update embl repbase table joining script

---
+++ misc/scripts/embl_repbase_match.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly = TRUE)
+library(tidyverse)
+library(stringr)
+
+# blast_dir <- "/Users/shunhua/work/S2/te_library_0521/embl_repbase_best.tsv"
+# te_table_dir <- "/Users/shunhua/git/transposons/misc/te_id_table_new.csv"
+# out_dir="/Users/shunhua/git/transposons/misc/embl_repbase_match.csv"
+
+blast_dir <- args[1]
+te_table_dir <- args[2]
+out_dir <- args[3]
+
+blast_hit <- read_tsv(blast_dir, col_names = c("fb_id", "repbase_name"))
+te_table_dir <- read_csv(te_table_dir, col_names = c("embl_id", "fb_id", "source_id", "flybase_name", "species", "te_name", "te_type_gff", "te_type_fasta", "te_subtype_fasta"))
+
+embl_repbase_match <-full_join(te_table_dir, blast_hit, by = "fb_id")
+embl_repbase_match %>% filter(is.na(repbase))
+
+write_csv(embl_repbase_match, out_dir)
