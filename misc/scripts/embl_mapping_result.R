2b95aeac431af7acda984892b1b870197280d0b5
Shunhua Han <hanshunhua0829@gmail.com>
2018-06-28 15:59:26 -0400

    debug

--- misc/scripts/embl_mapping_result.R
+++ misc/scripts/embl_mapping_result.R
@@ -8,7 +8,7 @@ te_table_dir <- args[2]
 out_dir <- args[3]
 
 blast_hit <- read_tsv(blast_repbase_dir, col_names = c("fb_id", "db_name", "p_ident", "n_ident", "align_len", "query_len", "subject_len"))
-te_table_dir <- read_csv(te_table_dir)
+te_meta <- read_csv(te_table_dir) %>% filter(species=="Dmel")
 
-table_join <-full_join(te_table_dir, blast_hit, by = "fb_id")
+table_join <-full_join(te_meta, blast_hit, by = "fb_id")
 write_csv(table_join, out_dir)
